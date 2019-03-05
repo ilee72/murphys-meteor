5eb552918d0f995a637259c12bce79fdc611fb9a
ilee72 <46728285+ilee72@users.noreply.github.com>
2019-03-05 11:19:08 -1000

    Murphys Complete

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopMenu from '../components/TopMenu';
+import MiddleMenu from '../components/MiddleMenu';
+import BottomMenu from '../components/BottomMenu';
+
+export default class Murphys extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <MiddleMenu/>
+        </div>
+    );
+  }
+}
